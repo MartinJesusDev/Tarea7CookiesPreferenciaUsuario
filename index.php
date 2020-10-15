c01e17970bc9ff8216671018133021d0e5eb3823
MartinJesusDev <xdddd23@gmail.com>
2020-10-15 22:01:45 +0200

    Creado formulario para usuario

--- index.php
+++ index.php
@@ -9,6 +9,40 @@
 </head>
 
 <body>
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <fieldset>
+      <legend>Deportes</legend>
+      <p>
+        <input type="checkbox" name="deportes[]" value="Ciclismo">Ciclismo
+        <input type="checkbox" name="deportes[]" value="Senderismo">Senderismo
+        <input type="checkbox" name="deportes[]" value="Fútbol">Fútbol
+      </p>
+    </fieldset>
+
+    <fieldset>
+      <legend>Música</legend>
+      <p>
+        <input type="checkbox" name="musica[]" value="Rock">Rock
+        <input type="checkbox" name="musica[]" value="Pop">Pop
+        <input type="checkbox" name="musica[]" value="Trap">Trap
+        <input type="checkbox" name="musica[]" value="Rap">Rap
+      </p>
+    </fieldset>
+
+    <fieldset>
+      <legend>Programación</legend>
+      <p>
+        <input type="checkbox" name="programacion[]" value="PHP">PHP
+        <input type="checkbox" name="programacion[]" value="JAVA">JAVA
+        <input type="checkbox" name="programacion[]" value="PYTHON">PYTHON
+      </p>
+    </fieldset>
+
+    <p>
+      <input type="submit" name="Enviar" value="Enviar">
+    </p>
+
+  </form>
 
 </body>
 
